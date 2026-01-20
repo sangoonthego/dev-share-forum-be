2a4846e3191088743661e061a26dfafb543c85f2
sangoonthego <knkidngoc@gmail.com>
2026-01-20 18:06:04 +0700

    feat(oauth2): implement github and google authentication module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
 import { TokenService } from './services/token.service';
@@ -10,9 +11,14 @@ import { LoginAuditService } from './services/login-audit.service';
 import { ChangePasswordService } from './services/change-password.service';
 import { AtStrategy } from './strategies/at.strategy';
 import { RtStrategy } from './strategies/rt.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { GitHubStrategy } from './strategies/github.strategy';
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [
+    PassportModule, // For OAuth2 strategies
+    JwtModule.register({}),
+  ],
   controllers: [AuthController],
   providers: [
     AuthService,
@@ -24,6 +30,8 @@ import { RtStrategy } from './strategies/rt.strategy';
     ChangePasswordService,
     AtStrategy, 
     RtStrategy,
+    GoogleStrategy, // OAuth2 strategies
+    GitHubStrategy,
   ],
   exports: [UserService, LoginAuditService], // Export for other modules
 })
